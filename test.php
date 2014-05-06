701815135ad83cbb2563b5a1ec1b4a8ca303aaba
John Youi <igonejack@gmail.com>
2014-05-07 04:39:52 +0800

    10

--- test.php
+++ test.php
@@ -1,4 +1,4 @@
 <?php
-	if (fopen(http://37.media.tumblr.com/6b2b589b9a575c2243ff60542aeadd2a/tumblr_mwqkjdMQwz1qioo2oo1_250.jpg, 'r')) {
+	if (fopen('http://37.media.tumblr.com/6b2b589b9a575c2243ff60542aeadd2a/tumblr_mwqkjdMQwz1qioo2oo1_250.jpg', 'r')) {
 	echo 'hello';
 	}
