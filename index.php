59ffbac93eafa055f3944f2cfef888f22d797997
John Youi <igonejack@gmail.com>
2014-05-07 01:20:07 +0800

    second

--- index.php
+++ index.php
@@ -2,6 +2,11 @@
 	$url = $_GET['url'];
 	include_once('simple_html_dom.php');
 	$html = new simple_html_dom($url);
-	$img = $html->find('ol#posts img', 0);
-	$src = $img->src;
-	header('location: ' . $src);
+
+	$images = $html->find('img');
+	foreach ($images as $img) {
+		if (preg_match("/.+media\.tumblr\.com.+((1280)|(500))\.(png)?(jpg)?$/", $img->src)) {
+			header('location: ' . $img->src);
+			break;
+		}
+	}
