303aaa323b99eb85f91531ba87b74078ccfcc52e
gonejack <igonejack@gmail.com>
2014-09-29 10:31:31 +0800

    New Version

--- fetch.php
+++ fetch.php
@@ -3,21 +3,81 @@
         echo 'hello';
         exit();
     }
+    
+    $blog = $_GET['blog'];
+    $content = getContent($_GET['url']);
+	$src = getSrc($content);
 
-    $pattern = '@(?:(?:content=")|(?:src="))(https?://[\S]+?media\.tumblr\.com[\S]+?_)((?:1280)|(?:500)|(?:400)|(?:250))(\.(?:png)?(?:jpg)?)"@';
-    $content = file_get_contents($_GET['url']);
-
-    if ($content) {
-
-        preg_match($pattern, $content, $matches);
-
-        $src = $matches[0];
-        
-        $img1280 = preg_replace($pattern, '${1}1280${3}', $src);
-        $img500 = preg_replace($pattern, '${1}500${3}', $src);
-
-        $src = preg_replace($pattern, '${1}${2}${3}', $src);
-
-        header('Content-Type:'.'image/jpeg');
-        readfile($src);
+    if ($content && $src) {
+        header('Location: '. $src, true, 301);
+        echo 'gotten!';
+        //logInfo($src, $blog);
+    } else {
+    	//logError($src, $content, $_GET['url'], parseUrl($_GET['url']));
+    	echo 'seems something wrong';
     }
+    
+    if (isset($_GET['forward'])) {
+    	file_get_contents($_GET['forward'].'?'.$_SERVER['QUERY_STRING']);
+    }
+    
+    
+    function parseUrl($origin) {
+    	
+    	preg_match('@http.+/post/\d+@', $origin, $temp);
+    	return $temp[0];
+    	
+    }
+    
+    function getContent($url) {
+    	$content = file_get_contents($url);
+    	if (!$content || strlen($content) < 100) {
+    		$content = file_get_contents(parseUrl($url));
+    	}
+    	
+    	return $content;
+    }
+    
+    function getSrc($content) {
+    	
+    	$pattern = '@(?:(?:content=")|(?:src="))(https?://[\S]+?media\.tumblr\.com[\S]+?_)((?:1280)|(?:500)|(?:400)|(?:250))(\.(?:png)?(?:jpg)?)"@';
+    	
+    	preg_match($pattern, $content, $matches);
+    	
+    	return preg_replace($pattern, '${1}${2}${3}', $matches[0]);
+    	
+    }
+    
+    function logInfo($src, $blog) {
+    	
+    	$imageTag = 
+	    			'<div>
+	    				<img src="'.$src.'">'
+	    			.'<p>'
+	    				.'Time '.date('Y-m-d H:m:s').'<br>'
+	    				."Source $src<br>"
+	    				."Blog $blog"
+	    			.'</p>'
+	    			.'</div>';
+    	
+    	file_put_contents('images.html', $html.$imageTag, FILE_APPEND);
+    	
+    	$filename = basename($src);
+    	if (!file_exists($filename)) {
+    		file_put_contents($filename, fopen($src, 'r'));
+    	}
+    }
+
+	function logError($src, $content, $originUrl, $parsedUrl) {
+
+		$error = '<p>'
+				.'Time '.date('Y-m-d H:m:s').'<br>'
+				.'Source '.($src ? $src : 'False').'<br>'
+				.'Content '.($content ? strlen($content) : 'False').'<br>'
+				.'OriginalURL '.$originUrl.'<br>'
+				."ParsedURL $parsedUrl"
+				.'</p>';
+		
+		file_put_contents('errors.html', $html.$error, FILE_APPEND);
+		
+	}
