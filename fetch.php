2c295eeb831456f1acc5806d9ed582ed415b23e0
gonejack <igonejack@gmail.com>
2015-04-16 14:16:05 +0800

    minor fix

--- fetch.php
+++ fetch.php
@@ -1,101 +1,118 @@
 <?php
 
-if (empty($_GET['url'])) {
-    exit('Hello World! fetch more images');
-}
+main();
 
-$htmlSource     = getContent($_GET['url']);
-$imageSources   = getSrcArray($htmlSource);
-$numberOfImages = count($imageSources);
+function main() {
+    (!isset($_GET['url']) || !filter_var($_GET['url'], FILTER_VALIDATE_URL)) && exit('Hello World!');
 
-if (!$htmlSource || $numberOfImages === 0) {
+    $strPageSource = getPageSource($_GET['url']);   #get HTML page source code
+    !$strPageSource && echoImageNotFoundTextFileAndExit($_GET['url']);
 
-    echo 'seems something wrong';
+    $arrImagesUrls = parseImagesUrls($strPageSource);   #parse urls of images
 
-} else {
+    $intCountOfImagesUrls = count($arrImagesUrls);
+    $intCountOfImagesUrls === 0 && echoImageNotFoundTextFileAndExit($_GET['url']);  #no image url found, echo error message as txt file.
+    $intCountOfImagesUrls === 1 && redirectAndExit(array_pop($arrImagesUrls));  #we got just one image url to be fetch, so no need for fetching, just redirect the browser to it.
 
-    if ($numberOfImages === 1) {
-        header('Location: ' . $imageSources[0], true, 301);
-        exit;
-    }
 
-    $imageZipAndValidSrc = getImageZipAndValidSrc($imageSources);
+    $arrContentAndUrlOfValidImages = fetchImages($arrImagesUrls); #not every url is available, so try every one.
 
-    if (count($imageZipAndValidSrc['validSrc']) === 1) {
-        header('Location: ' . $imageZipAndValidSrc['validSrc'][0], true, 301);
-        exit;
-    }
+    $intCountOfValidImagesUrls = count($arrContentAndUrlOfValidImages['validImagesUrls']);    #check out the number of available urls
+    $intCountOfValidImagesUrls === 0 && echoImageNotFoundTextFileAndExit($_GET['url']);
+    $intCountOfValidImagesUrls === 1 && redirectAndExit(array_pop($arrContentAndUrlOfValidImages['validImagesUrls']));    #if we got just one available url, no need to pack the image cause we could just redirect the browser.
 
-    header('Content-Type: application/zip');
-    header('Content-Length: ' . strlen($imageZipAndValidSrc['zipFile']));
-    header('Content-Disposition: attachment; filename=' . date('Y/M/j/D G:i:s') . '.zip');
-
-    echo $imageZipAndValidSrc['zipFile'];
-
+    //when we got multiple images to deal with
+    $strZipString = makeZipPack($arrContentAndUrlOfValidImages['imageStrings'], $arrContentAndUrlOfValidImages['validImagesUrls']);
+    outputZipPackAsFileDownload($strZipString);
 }
 
 /**
- * Parse URL
- * tumblr has two type of url
- * first type like http://blabla/digits
- * second type like http://blabla/digits/blabla
- * when you can't load content from second type, try first type
- *
- * @param $origin string Original URL(second type)
- * @return mixed
+ * get HTML page source
+ * @param $strUrl
+ * @return bool|string
  */
-function parseUrl($origin) {
-    preg_match('<http.+/post/\d+>', $origin, $new);
+function getPageSource($strUrl) {
 
-    return $new[0];
-}
+    $strPageSource = @file_get_contents($strUrl);
 
-/**
- * Get HTML content
- * failed first time would try to convert URL to fetch content
- * @param $url
- * @return string|false
- */
-function getContent($url) {
-    $content = @file_get_contents($url);
-    if (strlen($content) < 100) {
-        $content = @file_get_contents(parseUrl($url));
+    //Tumblr has two URL types, try the short one when the long one failed to be access.
+    if (strlen($strPageSource) < 100) {
+
+        $arrTemp     = preg_match('<http.+/post/\d+>', $strUrl, $new);
+        $strShortUrl = $arrTemp[0];
+
+        $strPageSource = @file_get_contents($strShortUrl);
+
+        //check one more time
+        strlen($strPageSource) < 100 && $strPageSource = false;
     }
 
-    return $content;
+    return $strPageSource;
 }
 
 /**
- * Get all URL of largest images
- * @param $content String
+ * regular expression fetching operation for images urls on HTML page source
+ * @param $strPageSource
  * @return array
  */
-function getSrcArray($content) {
+function parseImagesUrls($strPageSource) {
 
-    $patten = "<(?:content|src)=\"((?:https?://\d+\.media\.tumblr\.com)/(?:(\w+)/)?(?:tumblr_\w+_(1280|540|500|400|250)\.(?:png|jpg|gif)))\">i";
+    $arrReturnUrls = array();
 
-    $return = array();
+    $strRegPatten = "<(?:content|src)=\"((?:https?://\d+\.media\.tumblr\.com)/(?:(\w+)/)?(?:tumblr_\w+_(1280|540|500|400|250)\.(?:png|jpg|gif)))\">i";
 
-    if (preg_match_all($patten, $content, $matches)) {
+    if (preg_match_all($strRegPatten, $strPageSource, $arrMatches)) {
 
-        list(, $srcList, $hashList, $sizeList) = $matches;
+        $arrTemp = array(); #array( hashValue => array('url' => url, 'size' => size), hashValue => array('url' => url, 'size' => size),...)
 
-        $temp = array();
-        foreach ($srcList as $index => $src) {
-            $hash = $hashList[$index];
-            $size = $sizeList[$index];
+        list(, $arrUrls, $arrHashes, $arrSizes) = $arrMatches;
 
-            if (empty($temp[$hash]) || $size > $temp[$hash]['size']) {
-                $temp[$hash] = array('src' => $src, 'size' => $size);
+        //filter, find out the url which represent the max size of the image.
+        for ($i = 0, $length = sizeof($arrUrls); $i < $length; $i++) {
+
+            $strUrl    = $arrUrls[$i];
+            $strHashes = $arrHashes[$i];
+            $strSize   = $arrSizes[$i];
+
+            if (empty($arrTemp[$strHashes]) || $arrTemp[$strHashes]['size'] < $strSize) {
+                $arrTemp[$strHashes] = array('url' => $strUrl, 'size' => $strSize);
             }
         }
 
-        foreach ($temp as $item) {
-            $return[] = $item['src'];
+        foreach ($arrTemp as $arrItem) {
+            $arrReturnUrls[] = $arrItem['url'];
         }
     }
 
-    return $return;
+    return $arrReturnUrls;
+}
+
+/**
+ * get images raw strings
+ * @param $arrImagesUrls
+ * @return array
+ */
+function fetchImages($arrImagesUrls) {
+
+    $arrReturn = array('imageStrings' => array(), 'validImagesUrls' => array());
+
+    $arrValidStatus = array(200, 301, 304);
+
+    foreach ($arrImagesUrls as $strImageUrl) {
+
+        $strImageString = @file_get_contents($strImageUrl);
+        $intHttpStatus  = parseHeaders($http_response_header, 'status');
+
+        $boolFetchSuccess = in_array($intHttpStatus, $arrValidStatus);
+
+        if ($boolFetchSuccess) {
+            $arrReturn['imageStrings'][]    = $strImageString;
+            $arrReturn['validImagesUrls'][] = $strImageUrl;
+        }
+
+    }
+
+    return $arrReturn;
 }
 
 /**
@@ -131,27 +148,59 @@ function parseHeaders(array $headers, $header = null) {
 }
 
 /**
- * Download images, pack into a zip, return as zip string.
- * @param Array $imageSources
- * @return Array
+ * redirect the browser to the direct image url
+ * @param $strImageUrl
  */
-function getImageZipAndValidSrc($imageSources) {
+function redirectAndExit($strImageUrl) {
+    header('Location: ' . $strImageUrl, true, 301);
+    exit;
+}
+
+/**
+ * make a txt file including error message
+ * @param $strUrl
+ */
+function echoImageNotFoundTextFileAndExit($strUrl) {
+
+    header('Content-Type: text/html');
+    header('Content-Disposition: attachment; filename=' . date('Y/M/j/D G:i:s') . '.htm');
+
+    echo "No tumblr images found at <a href='$strUrl' target='_self'><i>$strUrl</i></a>";
+
+    exit;
+}
+
+/**
+ * generate zip file stream
+ * @param $arrImageStrings
+ * @param $arrImageUrls
+ * @return string
+ */
+function makeZipPack($arrImageStrings, $arrImageUrls) {
     require_once('zip.lib.php');
+    $zipGenerator = new ZipFile();
 
-    $zip      = new ZipFile();
-    $validSrc = array();
-    foreach ($imageSources as $source) {
+    for ($i = 0, $length = sizeof($arrImageStrings); $i < $length; $i++) {
 
-        $image = file_get_contents($source);
+        $strImageString = $arrImageStrings[$i];
+        $strImageUrl    = $arrImageUrls[$i];
 
-        if (in_array(parseHeaders($http_response_header, 'status'), array(200, 301, 304))) {
-
-            $zip->addFile($image, basename($source));
-            $validSrc[] = $source;
-
-        }
+        $zipGenerator->addFile($strImageString, basename($strImageUrl));
 
     }
 
-    return array('zipFile' => $zip->file(), 'validSrc' => $validSrc);
+    return $zipGenerator->file();
+}
+
+/**
+ * make some headers for zip file as attachment download
+ * @param $strZipString
+ */
+function outputZipPackAsFileDownload($strZipString) {
+
+    header('Content-Type: application/zip');
+    header('Content-Length: ' . strlen($strZipString));
+    header('Content-Disposition: attachment; filename=' . date('Y/M/j/D G:i:s') . '.zip');
+
+    echo $strZipString;
 }
